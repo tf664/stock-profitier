8d630533b56eef3888b130ba6c435674644766a8
tf664 <tilfeldkamp@gmail.com>
2026-07-24 16:51:06 +0200

    fix color settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,6 @@
 </script>
 
 <main>
-	<div id="top"></div>
 	<div class="main-banner">
 		<div style="margin: 2rem">
 			<button onclick={() => (showDataEntry = !showDataEntry)} class="btn-standard">
@@ -24,28 +23,27 @@
 			{#if showDataEntry}
 				<NewDataEntry onClose={() => (showDataEntry = false)} />
 			{/if}
-		</div>
-
-		<div>
-			<div class="z-1 title-wrapper">
-				<button
-					type="button"
-					class="select-none settings-title"
-					onclick={() => debugModeComponent?.handleTitleTap()}
-					aria-label="Activate Debug Mode"
-				>
-					Debug Mode
-				</button>
+			<div>
+				<div class="z-1 title-wrapper">
+					<button
+						type="button"
+						class="select-none settings-title"
+						onclick={() => debugModeComponent?.handleTitleTap()}
+						aria-label="Activate Debug Mode"
+					>
+						Debug Mode
+					</button>
+				</div>
+				<DebugMode bind:this={debugModeComponent} />
 			</div>
-			<DebugMode bind:this={debugModeComponent} />
-		</div>
 
-		<h3 class="font-semibold text-lg text-(--secondary)">Graphes</h3>
-
-		<div class="flex flex-col gap-4 p-4">
-			<PortfolioPieChart />
-			<MonthlyVolumeChart />
-			<OpenPositionsChart />
+			<br />
+			<h3 class="font-semibold text-primary text-2xl">Graphes</h3>
+			<div class="flex flex-col gap-4 p-4">
+				<PortfolioPieChart />
+				<MonthlyVolumeChart />
+				<OpenPositionsChart />
+			</div>
 		</div>
 	</div>
 </main>
